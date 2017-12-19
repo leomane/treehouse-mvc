1e233d02bdf106056dbc026e35eb500be9a7d751
Anthony Leo <aleo@plex.com>
2017-12-19 09:52:46 -0500

    Updated Comic Book Detail View to be strongly typed

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ComicBookGallery.Models;
 
 namespace ComicBookGallery.Controllers
 {
@@ -10,19 +11,22 @@ namespace ComicBookGallery.Controllers
   {
     public ActionResult Detail()
     {
-      ViewBag.SeriesTitle = "The Amazing Spider-Man";
-      ViewBag.IssueNumber = 700;
-      ViewBag.Description = "<p>Final issue!Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
-      ViewBag.Artists = new string[]
+      var comicBook = new ComicBook()
       {
-        "Script: Dan Slott",
-        "Pencils: Humberto Ramos",
-        "Inks: Victor Olazaba",
-        "Colors: Edgar Delgado",
-        "Letters: Chris Eliopoulos"
+        SeriesTitle = "The Amazing Spider-Man",
+        IssueNumber = 700,
+        DescriptionHtml = "<p>Final issue!Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+        Artists = new Artist[]
+        {
+          new Artist() { Role = "Script", Name = "Dan Slott" },
+          new Artist() { Role = "Pencils", Name = "Humberto Ramos" },
+          new Artist() { Role = "Inks", Name = "Victor Olazaba" },
+          new Artist() { Role = "Colors", Name = "Edgar Delgado" },
+          new Artist() { Role = "Letters", Name = "Chris Eliopoulos" }
+        }
       };
 
-      return View();
+      return View(comicBook);
     }
   }
 }
